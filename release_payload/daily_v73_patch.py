4007e7f908acbe336649d7cffd8881e631a7363d
UltimateSRB <krsticmiroslav99@gmail.com>
2026-08-23 19:17:31 +0200

    Use split readable Daily recovery components

--- release_payload/daily_v73_patch.py
+++ release_payload/daily_v73_patch.py
@@ -3,11 +3,16 @@
 from daily_v73_core import apply_daily_core_patch
 from daily_v73_identify import apply_daily_identify_patch
 from daily_v73_anchor import apply_daily_anchor_patch
-from daily_v73_recovery import apply_daily_recovery_patch
+from daily_v73_start import apply_daily_start_patch
+from daily_v73_route import apply_daily_route_patch
+from daily_v73_finish import apply_daily_finish_patch
+
 
 def apply_daily_v73_patch(text):
     text = apply_daily_core_patch(text)
     text = apply_daily_identify_patch(text)
     text = apply_daily_anchor_patch(text)
-    text = apply_daily_recovery_patch(text)
+    text = apply_daily_start_patch(text)
+    text = apply_daily_route_patch(text)
+    text = apply_daily_finish_patch(text)
     return text
